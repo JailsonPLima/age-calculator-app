9fcdd681c05afe0f066bb086fc4e26c948d84283
Jailson P. Lima <jailsonp.lima03@gmail.com>
2023-05-23 11:45:40 -0300

    feat: get current date with an api

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,15 @@
-console.log("start");
+const getCurrentDate = async () => {
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  const response = await fetch(
+    `https://worldtimeapi.org/api/timezone/${timezone}`
+  );
+
+  const data = await response.json();
+
+  const date = new Date(data.utc_datetime);
+
+  return date;
+};
+
+console.log(getCurrentDate());
